c304b57520df7e3879412efde08e71b2770ad77d
Raznic <raznic@users.noreply.github.com>
2019-03-04 11:16:32 -0500

    Record navbar drop-down menu.
    * Changes navbar to sport a drop-down menu for the different record
      types. This will help keep the navbar from getting too crowded as
      support for more resource records becomes available.

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,4 +1,7 @@
-// Initialize tooltips
 $(document).ready(function(){
     $('.tooltipped').tooltip();
+    $(".dropdown-trigger").dropdown({
+        hover: true,         // Trigger down-down on hover
+        coverTrigger: false  // Don't cover element that triggered drop-down, i.e. display drop-down beneath element
+    });
 });
